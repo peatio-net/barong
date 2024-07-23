44a734887fdf84841d21421f26c11a0b3c63b532
dnvu <dnvu.24h@gmail.com>
2024-07-23 22:30:07 +0700

    update values for session_create

--- app/api/v2/identity/utils.rb
+++ app/api/v2/identity/utils.rb
@@ -117,7 +117,9 @@ module API::V2
                         record: {
                           user: user.as_json_for_event_api,
                           user_ip: remote_ip,
-                          user_agent: request.env['HTTP_USER_AGENT']
+                          user_agent: request.env['HTTP_USER_AGENT'],
+                          user_ip_country: Barong::GeoIP.info(ip: remote_ip, key: :country),
+                          created_at: Time.current
                         })
       end
     end
